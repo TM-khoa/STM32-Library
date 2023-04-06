155e8f41c45f9f8bdf708b1e236d55bb9d9eb0e4
TM-khoa <minhkhoa9a31314@gmail.com>
2023-04-06 19:57:11 +0700

    Update HX711

--- Inc/HX711.h
+++ Inc/HX711.h
@@ -4,12 +4,11 @@
  *  Created on: Mar 31, 2023
  *      Author: Dien
  */
-
 #ifndef INC_HX711_H_
 #define INC_HX711_H_
 
 #include "main.h"
-
+#ifdef CONFIG_USE_HX711
 typedef struct{
 	GPIO_TypeDef *port;
 	uint16_t pin;
@@ -19,6 +18,7 @@ typedef struct{
 	pinconfig SCK;
 	pinconfig Data;
 	uint32_t buffer;
+	uint32_t EXTI_LINE;
 }HX711;
 
 typedef enum{
@@ -26,14 +26,24 @@ typedef enum{
 	HX711_Data,
 }pinName;
 
-#define clkPort dev->SCK.port
-#define clkPin dev->SCK.pin
-#define dtPort dev->Data.port
-#define dtPin dev->Data.pin
-#define buffer dev->buffer
+#define clkPort devTemp->SCK.port
+#define clkPin devTemp->SCK.pin
+#define dtPort devTemp->Data.port
+#define dtPin devTemp->Data.pin
+#define buffer devTemp->buffer
+#define HX711_EXTI_line devTemp->EXTI_LINE
 
-#define HX711_WRITE(PIN,LOGIC) (((PIN) == HX711_SCK)? HAL_GPIO_WritePin(clkPort,clkPin,(LOGIC)) : 0)
-#define HX711_READ(PIN) (((PIN) == HX711_Data)? HAL_GPIO_ReadPin(dtPort,dtPin) 	:  0)
-HAL_StatusTypeDef HX711_ConfigOnePin(HX711* dev,GPIO_TypeDef *port,uint16_t pin, pinName pinName);
-void HX711_readvalue (HX711* dev,pinName pinname);
+#define HX711_ENABLE_DOUT_INTERRUPT (HAL_NVIC_EnableIRQ(HX711_EXTI_line))
+#define HX711_DISABLE_DOUT_INTERRUPT (HAL_NVIC_DisableIRQ(HX711_EXTI_line))
+#define HX711_SCK_WRITE(LOGIC) (HAL_GPIO_WritePin(clkPort,clkPin,(LOGIC)))
+#define HX711_DATA_READ (HAL_GPIO_ReadPin(dtPort,dtPin))
+
+#define HX711_CALIB_OFFSET
+#define HX711_AVERAGE_SAMPLE 10
+
+HAL_StatusTypeDef HX711_AssignPin(HX711* dev,GPIO_TypeDef *port,uint16_t pin, pinName pinName);
+HAL_StatusTypeDef HX711_AssignEXTILine(HX711* dev,IRQn_Type IRQ);
+uint32_t HX711_ReadValue();
+void HX711_SetTarget(HX711* dev);
 #endif /* INC_HX711_H_ */
+#endif
--- Src/HX711.c
+++ Src/HX711.c
@@ -6,10 +6,21 @@
  */
 
 
+/**
+ * Một số lỗi khi sử dụng thư viện:
+ * Chưa khởi tạo biến đối tượng HX711, trả về HAL_ERROR
+ * Tắt sai LINE ngắt
+ * Chưa bật LINE ngắt
+ *
+ */
+
 #include "HX711.h"
 
+#ifdef CONFIG_USE_HX711
 
-HAL_StatusTypeDef HX711_ConfigOnePin(HX711* dev,GPIO_TypeDef *port,uint16_t pin, pinName pinName)
+HX711* devTemp;
+
+HAL_StatusTypeDef HX711_AssignPin(HX711* dev,GPIO_TypeDef *port,uint16_t pin, pinName pinName)
 {
 	if(!dev) return HAL_ERROR;
 	switch(pinName){
@@ -22,20 +33,48 @@ HAL_StatusTypeDef HX711_ConfigOnePin(HX711* dev,GPIO_TypeDef *port,uint16_t pin,
 		dev->Data.pin = pin;
 		break;
 	}
+
 	return HAL_OK;
 }
 
-void HX711_readvalue (HX711* dev,pinName pinname){
-	HX711_WRITE(HX711_SCK,0);
-	buffer = 0;
-	while(HX711_READ(HX711_Data) == 1);
-	for (int i = 0; i<24; i++){
-		HX711_WRITE(HX711_SCK,1);
-		buffer = buffer<<1;
-		HX711_WRITE(HX711_SCK,0);
-		if (HX711_READ(HX711_Data)) buffer++;
-	}
-	HX711_WRITE(HX711_SCK,1);
-	buffer = buffer^0x8000;
-	HX711_WRITE(HX711_SCK,0);
+HAL_StatusTypeDef HX711_AssignEXTILine(HX711* dev,IRQn_Type IRQ)
+{
+	if(!dev) return HAL_OK;
+	dev->EXTI_LINE = IRQ;
+	return HAL_OK;
 }
+
+void HX711_SetTarget(HX711* dev)
+{
+	while(!dev->EXTI_LINE);
+	devTemp = dev;
+}
+
+uint32_t HX711_ReadValue()
+{
+	while(!devTemp->EXTI_LINE);
+	HX711_DISABLE_DOUT_INTERRUPT;
+	HX711_SCK_WRITE(0);
+	buffer = 0;
+	for (int i = 0; i<24; i++){
+		HX711_SCK_WRITE(1);
+		buffer = buffer << 1;
+		HX711_SCK_WRITE(0);
+		if (HX711_DATA_READ) buffer++;
+	}
+	HX711_SCK_WRITE(1);
+	buffer = buffer^0x8000;
+	HX711_SCK_WRITE(0);
+	HX711_ENABLE_DOUT_INTERRUPT;
+	return buffer;
+}
+uint32_t HX711_ReadAverage()
+{
+	static uint32_t Val=0;
+	static uint8_t count=0;
+	if(count < HX711_AVERAGE_SAMPLE){
+		count = 0;
+		Val += HX711_ReadValue();
+	} else return Val/HX711_AVERAGE_SAMPLE;
+}
+#endif
