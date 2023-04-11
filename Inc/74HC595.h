b14f2aedb11e8ee49fb9f8d2b76e70273ad1493c
TM-khoa <minhkhoa9a31314@gmail.com>
2023-04-11 19:43:38 +0700

    fix ; in devTemp=dev of AssignPin

--- Inc/74HC595.h
+++ Inc/74HC595.h
@@ -20,12 +20,14 @@ typedef struct HC595{
 	pinConfig ds;
 	pinConfig clk;
 	pinConfig latch;
+	pinConfig oe;
 }HC595;
 
 typedef enum{
 	HC595_LATCH,
 	HC595_CLK,
-	HC595_DS
+	HC595_DS,
+	HC595_OE
 }pinName;
 
 #define latchPort devTemp->latch.port
@@ -34,14 +36,20 @@ typedef enum{
 #define clkPin devTemp->clk.pin
 #define dsPort devTemp->ds.port
 #define dsPin devTemp->ds.pin
+#define oePort devTemp->oe.port
+#define oePin devTemp->oe.pin
 
 #define HC595_WRITE(PIN,LOGIC) ( 	((PIN) == HC595_LATCH)? 	HAL_GPIO_WritePin(latchPort,latchPin,(LOGIC)) 	:	\
 									((PIN) == HC595_CLK) 	?	HAL_GPIO_WritePin(clkPort,clkPin,(LOGIC)) 		: 	\
+									((PIN) == HC595_OE) 	?	HAL_GPIO_WritePin(oePort,oePin,(LOGIC)) 		: 	\
 									((PIN) == HC595_DS) 	?	HAL_GPIO_WritePin(dsPort,dsPin,(LOGIC)) 		: 0	\
 								)
 
 HAL_StatusTypeDef HC595_Send_Data(uint16_t dt);
-HAL_StatusTypeDef HC595_TestPin(pinName pin);
+void HC595_TestPin(pinName pin);
+void HC595_Disable();
+void HC595_Enable();
 HAL_StatusTypeDef HC595_AssignPin(HC595* dev,GPIO_TypeDef *port,uint16_t pin, pinName pinName);
+void HC595_TestOutput();
 #endif /* INC_74HC595_H_ */
 #endif
--- Src/74HC595.c
+++ Src/74HC595.c
@@ -24,6 +24,10 @@ HAL_StatusTypeDef HC595_AssignPin(HC595* dev,GPIO_TypeDef *port,uint16_t pin, pi
 		dev->latch.port = port;
 		dev->latch.pin = pin;
 		break;
+	case HC595_OE:
+		dev->oe.port = port;
+		dev->oe.pin = pin;
+		break;
 	}
 	devTemp = dev;
 	return HAL_OK;
@@ -32,10 +36,19 @@ void HC595_TestPin(pinName pinName)
 {
 	while(!devTemp);
 	HC595_WRITE(pinName,1);
-	HAL_Delay(1000);
+	HAL_Delay(2000);
 	HC595_WRITE(pinName,0);
-	HAL_Delay(1000);
-	return HAL_OK;
+	HAL_Delay(2000);
+}
+
+void HC595_Enable()
+{
+	HC595_WRITE(HC595_OE,0);
+}
+
+void HC595_Disable()
+{
+	HC595_WRITE(HC595_OE,1);
 }
 
 HAL_StatusTypeDef HC595_Send_Data(uint16_t dt)
@@ -47,7 +60,6 @@ HAL_StatusTypeDef HC595_Send_Data(uint16_t dt)
 			HC595_WRITE(HC595_DS,1);
 		else
 			HC595_WRITE(HC595_DS,0);
-
 		HC595_WRITE(HC595_CLK,0);
 		HC595_WRITE(HC595_CLK,1);
 		dt <<= 1;
@@ -55,5 +67,13 @@ HAL_StatusTypeDef HC595_Send_Data(uint16_t dt)
 	HC595_WRITE(HC595_LATCH,0);
 	HC595_WRITE(HC595_LATCH,1);
 	return HAL_OK;
+}
+
+void HC595_TestOutput()
+{
+	HC595_Send_Data(0x55);
+	HAL_Delay(2000);
+	HC595_Send_Data(0x55<<1);
+	HAL_Delay(2000);
 }
 #endif
