d2414735c8ee5659c68c95f0c99e83b4c06cf6a9
TM-khoa <minhkhoa9a31314@gmail.com>
2023-09-25 18:30:30 +0700

    Update PID library

--- Inc/PID.h
+++ Inc/PID.h
@@ -6,57 +6,33 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-typedef struct OutVal{
-    double curr;
-    double pre;
-    double P;// set
-    double I;// set
-    double I_P;// previous intergration
-    double D;
-    double DF;//fitered derivative
-    double DF_P;//previous fitered derivative
-}OutVal;
-
-typedef struct Encoder{
-
-}Encoder;
-
 typedef struct PID_Param{
-    uint32_t setVal;
-    uint32_t deltaT;
-    double currVal;
-    double e;
-    double e_p;
-    double kp;
-    double ki;
-    double kd;
-    OutVal *u;
-    Encoder *enc;
+	//---------Input Parameters-----------//
+    float e;
+    float e_Pre;
+    float deltaT;
+	//---------Propotion Parameters-------//
+    float kP;
+    float uP;
+	//---------Intergral Parameters-------//
+    float kI;
+    float uI;
+    float uI_Pre;
+    float uI_AboveLimit;
+    float uI_BelowLimit;
+	//---------Derivative Parameters------//
+    float kD;
+    float uD;
+    float uD_Fil;
+    float uD_FilPre;
+    float alpha;
+	//---------Sum Parameters-------------//
+    float u;
+    float u_AboveLimit;
+    float u_BelowLimit;
+
 }PID_Param;
-
-
-#define uI_LLim 10
-#define uI_HLim -10
-#define u_LLim 10
-#define u_HLim -10
-
-#define setVal  pid->setVal
-#define currVal pid->currVal
-#define deltaT  pid->deltaT
-#define e       pid->e
-#define e_p     pid->e_p
-#define kp      pid->kp
-#define ki      pid->ki
-#define kd      pid->kd
-#define uCurr   pid->u->curr
-#define uPre    pid->u->pre
-#define uP      pid->u->P
-#define uI      pid->u->I
-#define uI_P    pid->u->I_P
-#define uD      pid->u->D
-#define uDF     pid->u->DF
-#define uDF_P   pid->u->DF_P
-
-int gunCalPID(PID_Param *pid);
+void PID_Init(PID_Param *pid);
+void PID_Cal(PID_Param *pid,double Target_set,double CurrVal_set);
 #endif
 #endif
--- Src/PID.c
+++ Src/PID.c
@@ -1,27 +1,59 @@
 #include "PID.h"
 #ifdef CONFIG_USE_PID
-int PID_cal(PID_Param *pid)
+
+#define e               pid->e
+#define e_Pre           pid->e_Pre
+#define deltaT          pid->deltaT
+#define kP              pid->kP
+#define uP              pid->uP
+#define kI              pid->kI
+#define uI              pid->uI
+#define uI_Pre          pid->uI_Pre
+#define kD              pid->kD
+#define uD              pid->uD
+#define uD_Fil          pid->uD_Fil
+#define uD_FilPre 		pid->uD_FilPre
+#define alpha   		pid->alpha
+#define u				pid->u
+#define u_AboveLimit 	pid->u_AboveLimit
+#define u_BelowLimit 	pid->u_BelowLimit
+#define uI_AboveLimit	pid->uI_AboveLimit
+#define uI_BelowLimit	pid->uI_BelowLimit
+
+/**
+ * @brief Tính toán giá trị điện áp xuất ra bằng công thức PID
+ * @param *pid con trỏ tới struct PID_Param
+ * @param Target_set giá trị xác lập mong muốn
+ * @param CurrVal_set giá trị feedback của hệ thống
+ * @return
+ */
+
+void PID_Cal(PID_Param *pid,double Target_set,double CurrVal_set)
 {
-	if(!pid) return -1;
-	double alpha=0;
-	e = setVal - currVal;
-	uP = kp*e;
-	uI = uI_P + ki*e*deltaT;
-	uI = uI > uI_HLim ? uI_HLim : 
-	    (uI < uI_LLim ? uI_LLim : uI);
-	uD = kd*(e - e_p)/deltaT;
-	uDF = (1-alpha)*uDF_P+alpha*uD;
-	e_p = e;
-	uI_P = uI;
-	uDF_P = uDF;
-	uCurr = uP+uI+uDF;
-	uCurr =  uCurr > u_HLim ? u_HLim : 
-	   		(uCurr < u_LLim ? u_LLim : uCurr);
-	*pwm = uCurr;
-	return 0;
+//-----------------------Input-------------------------//
+	e = Target_set - CurrVal_set;
+
+//-----------------------Propotion Term----------------//
+	uP = kP*e;
+
+//-----------------------Integral Term-----------------//
+	uI = uI_Pre + kI*e*deltaT;
+	uI = uI > uI_AboveLimit ? uI_AboveLimit : uI;
+	uI = uI < uI_BelowLimit ? uI_BelowLimit : uI;
+
+//-----------------------Derivative Term---------------//
+	uD = kD*(e - e_Pre)/deltaT;
+	uD_Fil = (1-alpha)*uD_FilPre+alpha*uD;
+
+//-----------------------Previous Value----------------//
+	e_Pre = e;
+	uI_Pre = uI;
+	uD_FilPre = uD_Fil;
+
+//-----------------------Sum---------------------------//
+	u = uP + uI + uD;
+	u = u > u_AboveLimit ? u_AboveLimit : u;
+	u = u < u_BelowLimit ? u_BelowLimit : u;
 }
 
-void PID_setPIDParam(PID_Param *pid){
-
-}
 #endif
